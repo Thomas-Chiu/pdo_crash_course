c902fc7ce4a307880af51f09079b9777acc28120
Thomas-Chiu <josemich0511@gmail.com>
2021-08-23 23:19:13 +0800

    query indent

--- php_rest_myblog/models/Post.php
+++ php_rest_myblog/models/Post.php
@@ -25,20 +25,19 @@ class Post
   {
     // 建立 query
     $query = "SELECT 
-          c.name as category_name, 
-          p.id, 
-          p.category_id, 
-          p.title, 
-          p.body, 
-          p.author, 
-          p.created_at 
-        FROM 
-          $this->table p 
-        LEFT JOIN 
-          categories c ON p.category_id = c.id 
-        ORDER BY 
-          p.created_at DESC";
-    // $query = "SELECT * FROM $this->table";
+      c.name as category_name, 
+      p.id, 
+      p.category_id, 
+      p.title, 
+      p.body, 
+      p.author, 
+      p.created_at 
+    FROM 
+      $this->table p 
+    LEFT JOIN 
+      categories c ON p.category_id = c.id 
+    ORDER BY 
+      p.created_at DESC";
 
     // 準備 statement
     $stmt = $this->conn->prepare($query);
